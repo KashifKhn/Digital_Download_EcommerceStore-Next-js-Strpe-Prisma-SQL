ed8958ce50af32da5c5480a2410b941e52ec8693
KashifKhn <kashifkhnx04@gmail.com>
2024-06-07 19:46:48 +0500

    feat: Add Prisma database connection and configuration
    This commit adds the `db.ts` file to the `src/db` directory, which contains the code for connecting to the Prisma database using the `PrismaClient` from the `@prisma/client` package. It also includes a singleton function `prismaClientSingleton` to ensure a single instance of the `PrismaClient` is used throughout the application. The `db` object is exported as the default export from the file. Additionally, the `globalThis.prisma` object is declared and assigned the value of `db` if it is not already defined. This commit improves the organization and configuration of the database connection in the project.

---
+++ src/db/db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prisma: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const db = globalThis.prisma ?? prismaClientSingleton();
+
+export default db;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
