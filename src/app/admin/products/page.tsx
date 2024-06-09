6c816ebf7af1a2fe4efb9e155cc678b8852b13a8
KashifKhn <kashifkhnx04@gmail.com>
2024-06-09 09:27:38 +0500

    feat: Add page header , new product add button and Products table to admin products page

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,10 +1,20 @@
+import { Button } from "@/components/ui/button";
+import PageHeader from "../_components/PageHeader";
+import Link from "next/link";
+import ProductsTable from "./_components/ProductsTable";
 
 const AdminProductsPage = () => {
-return (
+  return (
     <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button>
+          <Link href="admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+      <ProductsTable />
     </>
-)
-
+  );
 };
 
 export default AdminProductsPage;
