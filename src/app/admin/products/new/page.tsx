c023a98a2ebfdbfb919b1fe22ec9e1b7a4bb8b9b
KashifKhn <kashifkhnx04@gmail.com>
2024-06-09 10:11:28 +0500

    feat: Add NewProductPage component for creating new products in admin products page

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageHeader from "../../_components/PageHeader";
+import ProductForm from "../_components/ProductForm";
+
+const NewProductPage = () => {
+  return (
+    <>
+      <PageHeader>Add Products</PageHeader>
+      <ProductForm />
+    </>
+  );
+};
+
+export default NewProductPage;
