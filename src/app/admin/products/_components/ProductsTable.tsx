4c82a2a8999e1740b3dfcf8a18d89acde79492a0
KashifKhn <kashifkhnx04@gmail.com>
2024-06-11 19:10:43 +0500

    refactor: Update ProductsTable component to fetch  product data from the database

--- src/app/admin/products/_components/ProductsTable.tsx
+++ src/app/admin/products/_components/ProductsTable.tsx
@@ -1,7 +1,28 @@
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
 import React from "react";
 
-const ProductsTable = () => {
+const ProductsTable = async () => {
+  const products = await db.product.findMany({
+    select: {
+      name: true,
+      id: true,
+      isAvailableForPurchase: true,
+      priceInCent: true,
+      _count: { select: { Order: true } },
+    },
+    orderBy: { name: "asc" },
+  });
+
+  if (products.length === 0) {
+    return <p className="text-center">No products found.</p>;
+  }
   return (
     <Table>
       <TableHeader>
@@ -17,6 +38,7 @@ const ProductsTable = () => {
           </TableHead>
         </TableRow>
       </TableHeader>
+      <TableBody>{}</TableBody>
     </Table>
   );
 };
