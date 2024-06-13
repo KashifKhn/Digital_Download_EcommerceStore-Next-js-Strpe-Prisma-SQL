326c8253dea4ba8edb7a5d82371202bbce6b5c85
KashifKhn <kashifkhnx04@gmail.com>
2024-06-13 08:36:16 +0500

    refactor: extract  table header to a speare in ProductsTable component

--- src/app/admin/products/_components/ProductsTable.tsx
+++ src/app/admin/products/_components/ProductsTable.tsx
@@ -26,19 +26,6 @@ const ProductsTable = async () => {
   }
   return (
     <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-0">
-            <span className="sr-only">Available for Purchase</span>
-          </TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Price</TableHead>
-          <TableHead>Orders</TableHead>
-          <TableHead className="w-0">
-            <span className="sr-only">Actions</span>
-          </TableHead>
-        </TableRow>
-      </TableHeader>
       <ProductTableList products={products} />
     </Table>
   );
