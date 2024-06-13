543f8282e6edb6854d6e2f766905b428bf1b0757
KashifKhn <kashifkhnx04@gmail.com>
2024-06-13 06:33:08 +0500

    feat: Add ProductRow component for displaying a single row of product information

---
+++ src/app/admin/products/_components/ProductRow.tsx
@@ -0,0 +1,35 @@
+import { TableCell, TableRow } from "@/components/ui/table";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { CheckCircle2, XCircle } from "lucide-react";
+import React from "react";
+import ProductRowDropdownMenu from "./ProductRowDropdownMenu";
+
+type ProductRowProps = { product: Product };
+
+const ProductRow = ({ product }: ProductRowProps) => {
+  return (
+    <TableRow key={product.id}>
+      <TableCell>
+        {product.isAvailableForPurchase ? (
+          <>
+            {" "}
+            <CheckCircle2 /> <span className="sr-only">Available</span>{" "}
+          </>
+        ) : (
+          <>
+            {" "}
+            <XCircle /> <span className="sr-only">Unavailable</span>
+          </>
+        )}
+      </TableCell>
+      <TableCell>{product.name}</TableCell>
+      <TableCell>{formatCurrency(product.priceInCent / 100)}</TableCell>
+      <TableCell>{formatNumber(product._count.Order)}</TableCell>
+      <TableCell>
+        <ProductRowDropdownMenu product={product} />
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default ProductRow;
