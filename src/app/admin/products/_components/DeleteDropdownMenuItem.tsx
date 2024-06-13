2b48bf691da5d2a748aa38d14ed81d0c25ed88c5
KashifKhn <kashifkhnx04@gmail.com>
2024-06-13 06:12:39 +0500

    feat: Add DeleteDropdownMenuItem component for deleting a product

---
+++ src/app/admin/products/_components/DeleteDropdownMenuItem.tsx
@@ -0,0 +1,27 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import React, { useTransition } from "react";
+import { deleteProducts } from "../../_actions/products";
+
+const ToggleDropdownMenuItem = ({
+  id,
+  disabled,
+}: {
+  id: string;
+  disabled: boolean;
+}) => {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <DropdownMenuItem
+      disabled={isPending || disabled}
+      onClick={() => {
+        startTransition(async () => {
+          await deleteProducts(id);
+        });
+      }}
+    >
+      Delete
+    </DropdownMenuItem>
+  );
+};
+
+export default ToggleDropdownMenuItem;
