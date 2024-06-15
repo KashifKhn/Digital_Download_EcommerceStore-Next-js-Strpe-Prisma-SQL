810d3f0ece9fbfe559dacc1f3a2ff7623bf44616
KashifKhn <kashifkhnx04@gmail.com>
2024-06-15 23:28:51 +0500

    refactor: Remove Nav component and replace with AdminNav in AdminLayout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
-import Nav from "@/components/Nav";
 import React from "react";
+import AdminNav from "./_components/AdminNav";
 
 export const dynamic = "force-dynamic";
 
@@ -10,7 +10,7 @@ const AdminLayout = ({
 }>) => {
   return (
     <>
-      <Nav />
+      <AdminNav />
       <div className="my-6 container">{children}</div>
     </>
   );
