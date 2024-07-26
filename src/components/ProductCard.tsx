98d030b6e2a6ff472d4e4a961140912743083e0c
KashifKhn <kashifkhnx04@gmail.com>
2024-07-26 10:52:02 +0500

    refactor: Update ProductCard to fix typo in priceInCent prop

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,60 +1,58 @@
-import React from 'react'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
-import Image from 'next/image'
-import { Button } from './ui/button'
-import Link from 'next/link'
-import { formatCurrency } from '@/lib/formatters'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { formatCurrency } from "@/lib/formatters";
 
 type ProductCardProps = {
-  id: string
-  name: string
-  priceInCents: number
-  description: string
-  imagePath: string
-}
+  id: string;
+  name: string;
+  priceInCent: number;
+  description: string;
+  imagePath: string;
+};
 
 const ProductCard = ({
-    id,
-    name,
-    priceInCents,
-    description,
-    imagePath,
-  }: ProductCardProps) => {
+  id,
+  name,
+  priceInCent,
+  description,
+  imagePath,
+}: ProductCardProps) => {
   return (
     <Card className="flex overflow-hidden flex-col">
       <div className="relative w-full h-auto aspect-video">
-        <Image src={imagePath} fill alt={name} />
+        <Image
+          src={imagePath}
+          fill
+          alt={name}
+        />
       </div>
       <CardHeader>
         <CardTitle>{name}</CardTitle>
-        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
-      </CardHeader>
-      <CardContent className="flex-grow"><Card className="flex overflow-hidden flex-col">
-      <div className="relative w-full h-auto aspect-video">
-        <Image src={imagePath} fill alt={name} />
-      </div>
-      <CardHeader>
-        <CardTitle>{name}</CardTitle>
-        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+        <CardDescription>{formatCurrency(priceInCent / 100)}</CardDescription>
       </CardHeader>
       <CardContent className="flex-grow">
         <p className="line-clamp-4">{description}</p>
       </CardContent>
       <CardFooter>
-        <Button asChild size="lg" className="w-full">
+        <Button
+          asChild
+          size="lg"
+          className="w-full">
           <Link href={`/products/${id}/purchase`}>Purchase</Link>
         </Button>
       </CardFooter>
     </Card>
-        <p className="line-clamp-4">{description}</p>
-      </CardContent>
-      <CardFooter>
-        <Button asChild size="lg" className="w-full">
-          <Link href={`/products/${id}/purchase`}>Purchase</Link>
-        </Button>
-      </CardFooter>
-    </Card>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
