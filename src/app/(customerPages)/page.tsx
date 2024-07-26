481511f178e3f3ae0b62e17010d2fa2a0a4a3b5b
KashifKhn <kashifkhnx04@gmail.com>
2024-07-26 14:34:19 +0500

    refactor: Update customer page with cached product data

--- src/app/(customerPages)/page.tsx
+++ src/app/(customerPages)/page.tsx
@@ -1,28 +1,42 @@
 import db from "@/db/db";
 import React from "react";
 import ProductGridSection from "./_components/ProductGridSection";
+import { cache } from "@/lib/cache";
 
-const getNewestProduct = () => {
-  return db.product.findMany({
-    where: { isAvailableForPurchase: true },
-    orderBy: { createdAt: "desc" },
-    take: 6,
-  });
-};
-
-const getMostPopularProduct = () => {
-  return db.product.findMany({
-    where: { isAvailableForPurchase: true },
-    orderBy: { Order: { _count: "desc" } },
-    take: 6,
-  });
-};
+const getNewestProduct = cache(
+  () => {
+    return db.product.findMany({
+      where: { isAvailableForPurchase: true },
+      orderBy: { createdAt: "desc" },
+      take: 6,
+    });
+  },
+  ["/", "newest-products"],
+  { revalidate: 60 * 60 * 24 }
+);
+const getMostPopularProduct = cache(
+  () => {
+    return db.product.findMany({
+      where: { isAvailableForPurchase: true },
+      orderBy: { Order: { _count: "desc" } },
+      take: 6,
+    });
+  },
+  ["/", "most-popular-products"],
+  { revalidate: 60 * 60 * 24 }
+);
 
 const page = () => {
   return (
     <main className="space-y-12">
-      <ProductGridSection title="Most Popular" productsFetcher={getMostPopularProduct} />
-      <ProductGridSection title="Newest" productsFetcher={getNewestProduct} />
+      <ProductGridSection
+        title="Most Popular"
+        productsFetcher={getMostPopularProduct}
+      />
+      <ProductGridSection
+        title="Newest"
+        productsFetcher={getNewestProduct}
+      />
     </main>
   );
 };
