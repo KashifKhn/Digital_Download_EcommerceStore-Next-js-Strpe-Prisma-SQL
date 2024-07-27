841574cc248ea9845dff2943301d46b2f046ccc0
KashifKhn <kashifkhnx04@gmail.com>
2024-07-27 10:52:16 +0500

    feat: Add Form component for product purchase page

---
+++ src/app/(customerPages)/products/[id]/purchase/_components/Form.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { userOrderExists } from "@/app/actions/orders";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatCurrency } from "@/lib/formatters";
+import {
+  LinkAuthenticationElement,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import React, { FormEvent, useState } from "react";
+
+type FormProps = {
+  priceInCent: number;
+  productId: string;
+};
+
+const Form = ({ priceInCent, productId }: FormProps) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [email, setEmail] = useState<string>();
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (stripe == null || elements == null || email == null) return;
+
+    setIsLoading(true);
+
+    const orderExists = await userOrderExists(email, productId);
+
+    if (orderExists) {
+      setErrorMessage(
+        "You have already purchased this product. Try downloading it from the My Orders page"
+      );
+      setIsLoading(false);
+      return;
+    }
+
+    stripe
+      .confirmPayment({
+        elements,
+        confirmParams: {
+          return_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/stripe/purchase-success`,
+        },
+      })
+      .then(({ error }) => {
+        if (error.type === "card_error" || error.type === "validation_error") {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage("An unknown error occurred");
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Checkout</CardTitle>
+          {errorMessage && (
+            <CardDescription className="text-destructive">
+              {errorMessage}
+            </CardDescription>
+          )}
+        </CardHeader>
+        <CardContent>
+          <PaymentElement />
+          <div className="mt-4">
+            <LinkAuthenticationElement
+              onChange={(e) => setEmail(e.value.email)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button
+            className="w-full"
+            size="lg"
+            disabled={stripe == null || elements == null || isLoading}>
+            {isLoading
+              ? "Purchasing..."
+              : `Purchase - ${formatCurrency(priceInCent / 100)}`}
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+};
+
+export default Form;
