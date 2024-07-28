4469c4fa4f18cca86a3f32b4efdd9767f1f34a9f
KashifKhn <kashifkhnx04@gmail.com>
2024-07-28 10:59:27 +0500

    feat: Add OrdersPage component for displaying orders in admin panel

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "../_components/PageHeader";
+import OrdersTable from "./_components/OrdersTable";
+
+const OrdersPage = () => {
+  return (
+    <>
+      <PageHeader>Sales</PageHeader>
+      <OrdersTable />   
+    </>
+  );
+};
+
+export default OrdersPage;
