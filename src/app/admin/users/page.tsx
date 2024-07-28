50bfed9d67112f0b153dabb03f52a317e5ebe425
KashifKhn <kashifkhnx04@gmail.com>
2024-07-28 10:42:27 +0500

    feat: Add UsersPage component for displaying users in admin panel

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,13 @@
+import PageHeader from "../_components/PageHeader";
+import UsersTable from "./_components/UsersTable";
+
+const UsersPage = () => {
+  return (
+    <>
+      <PageHeader>Customers</PageHeader>
+      <UsersTable />
+    </>
+  );
+};
+
+export default UsersPage;
