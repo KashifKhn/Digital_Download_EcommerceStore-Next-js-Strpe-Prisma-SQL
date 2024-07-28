fd4755dc492d2d3a4b915a0d4731dede106f392e
KashifKhn <kashifkhnx04@gmail.com>
2024-07-28 10:42:19 +0500

    feat: Add UsersTable component for displaying users in admin panel

---
+++ src/app/admin/users/_components/UsersTable.tsx
@@ -0,0 +1,48 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
+import UserRow from "./UserRow";
+
+const getUsers = () => {
+  return db.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      orders: { select: { pricePaidInCents: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+};
+
+const UsersTable = async () => {
+  const users = await getUsers();
+
+  if (users.length === 0) return <p>No customers found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead>Value</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <UserRow user={user} />
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default UsersTable;
