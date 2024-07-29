045aaa75ca9cc5250d697eb23e81051de5abce7a
KashifKhn <kashifkhnx04@gmail.com>
2024-07-29 11:26:16 +0500

    feat: Add OrderHistoryEmail component for displaying order history in email template

---
+++ src/emails/OrderHistory.tsx
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import OrderInformation from "./components/OrderInformation";
+import React from "react";
+
+type OrderHistoryEmailProps = {
+  orders: {
+    id: string;
+    pricePaidInCents: number;
+    createdAt: Date;
+    downloadVerificationId: string;
+    product: {
+      name: string;
+      imagePath: string;
+      description: string;
+    };
+  }[];
+};
+
+const OrderHistoryEmail = ({ orders }: OrderHistoryEmailProps) => {
+  return (
+    <Html>
+      <Preview>Order History & Downloads</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Order History</Heading>
+            {orders.map((order, index) => (
+              <React.Fragment key={order.id}>
+                <OrderInformation
+                  order={order}
+                  product={order.product}
+                  downloadVerificationId={order.downloadVerificationId}
+                />
+                {index < orders.length - 1 && <Hr />}
+              </React.Fragment>
+            ))}
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+OrderHistoryEmail.PreviewProps = {
+  orders: [
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 10000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "Product name",
+        description: "Some description",
+        imagePath:
+          "uploads/products/images/b82d0e3d-0319-4e57-9ad2-3a7ad0a79621-87763.jpg",
+      },
+    },
+    {
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+      pricePaidInCents: 2000,
+      downloadVerificationId: crypto.randomUUID(),
+      product: {
+        name: "Product name 2",
+        description: "Some other desc",
+        imagePath:
+          "uploads/products/images/b82d0e3d-0319-4e57-9ad2-3a7ad0a79621-87763.jpg",
+      },
+    },
+  ],
+} satisfies OrderHistoryEmailProps;
+
+export default OrderHistoryEmail;
