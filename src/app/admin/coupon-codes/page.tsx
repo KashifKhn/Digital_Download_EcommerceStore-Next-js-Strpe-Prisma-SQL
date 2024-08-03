0f78b8560d5a91eae2c7549b7234894ee1e0b57e
KashifKhn <kashifkhnx04@gmail.com>
2024-08-03 23:49:31 +0500

    feat: Add CouponPage component for managing coupon codes

---
+++ src/app/admin/coupon-codes/page.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button";
+import PageHeader from "../_components/PageHeader";
+import Link from "next/link";
+import db from "@/db/db";
+import { Prisma } from "@prisma/client";
+import CouponTable from "./_components/CouponTable";
+
+const WHERE_EXPIRED: Prisma.CouponCodeWhereInput = {
+  OR: [
+    { limit: { not: null, lte: db.couponCode.fields.uses } },
+    { expiresAt: { not: null, lte: new Date() } },
+  ],
+};
+
+const SELECT_FIELDS: Prisma.CouponCodeSelect = {
+  id: true,
+  allProducts: true,
+  code: true,
+  discountAmount: true,
+  discountType: true,
+  expiresAt: true,
+  limit: true,
+  uses: true,
+  isActive: true,
+  products: { select: { name: true } },
+  _count: { select: { orders: true } },
+};
+
+const getExpiredCouponCodes = () => {
+  return db.couponCode.findMany({
+    select: SELECT_FIELDS,
+    where: WHERE_EXPIRED,
+    orderBy: { createdAt: "asc" },
+  });
+};
+
+export const getUnexpiredCouponCodes = () => {
+  return db.couponCode.findMany({
+    select: SELECT_FIELDS,
+    where: { NOT: WHERE_EXPIRED },
+    orderBy: { createdAt: "asc" },
+  });
+};
+
+const CouponPage = async () => {
+  const [expiredCouponCodes, unexpiredCouponCodes] = await Promise.all([
+    getExpiredCouponCodes(),
+    getUnexpiredCouponCodes(),
+  ]);
+
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Coupons</PageHeader>
+        <Button asChild>
+          <Link href="/admin/discount-codes/new">Add Coupon</Link>
+        </Button>
+      </div>
+      <CouponTable
+        coupons={expiredCouponCodes}
+        canDeactivate
+      />
+
+      <div className="mt-8">
+        <h2 className="text-xl font-bold">Expired Coupons</h2>
+        <CouponTable
+          coupons={unexpiredCouponCodes}
+          isInactive
+        />
+      </div>
+    </>
+  );
+};
+
+export default CouponPage;
