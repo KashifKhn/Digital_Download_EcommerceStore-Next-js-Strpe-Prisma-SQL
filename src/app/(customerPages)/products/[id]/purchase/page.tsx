8bc6782ed547db825e1fbb5d64da1dcd59d5df37
KashifKhn <kashifkhnx04@gmail.com>
2024-08-11 19:51:45 +0500

    feat: Update ProductPurchasePage to handle coupon codes

--- src/app/(customerPages)/products/[id]/purchase/page.tsx
+++ src/app/(customerPages)/products/[id]/purchase/page.tsx
@@ -2,13 +2,23 @@ import db from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
 import CheckoutForm from "./_components/CheckoutForm";
+import { usableDiscountCodeWhere } from "@/lib/couponCodeHelper";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
+const getDiscountCode = (coupon: string, productId: string) => {
+  return db.couponCode.findUnique({
+    select: { id: true, discountAmount: true, discountType: true },
+    where: { ...usableDiscountCodeWhere, code: coupon },
+  });
+};
+
 const ProductPurchasePage = async ({
   params: { id },
+  searchParams: { coupon },
 }: {
   params: { id: string };
+  searchParams: { coupon?: string };
 }) => {
   const product = await db.product.findUnique({ where: { id } });
   if (product == null) {
@@ -25,11 +35,15 @@ const ProductPurchasePage = async ({
     throw Error("Stripe failed to create the intent");
   }
 
+  const couponCode =
+    coupon == null ? undefined : await getDiscountCode(coupon, product.id);
+
   return (
     <>
       <CheckoutForm
         product={product}
         clientSecret={paymentIntent.client_secret}
+        couponCode={couponCode || undefined}
       />
     </>
   );
