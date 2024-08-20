e032e2c7311ecc297cc774e06d8611c221585b21
KashifKhn <kashifkhnx04@gmail.com>
2024-08-20 12:26:31 +0500

    refactor: Update coupon-codes/page.tsx to use couponService for retrieving coupon codes

--- src/app/admin/coupon-codes/page.tsx
+++ src/app/admin/coupon-codes/page.tsx
@@ -1,46 +1,11 @@
 import { Button } from "@/components/ui/button";
 import PageHeader from "../_components/PageHeader";
 import Link from "next/link";
-import db from "@/db/db";
-import { Prisma } from "@prisma/client";
 import CouponTable from "./_components/CouponTable";
-
-const WHERE_EXPIRED: Prisma.CouponCodeWhereInput = {
-  OR: [
-    { limit: { not: null, lte: db.couponCode.fields.uses } },
-    { expiresAt: { not: null, lte: new Date() } },
-  ],
-};
-
-const SELECT_FIELDS: Prisma.CouponCodeSelect = {
-  id: true,
-  allProducts: true,
-  code: true,
-  discountAmount: true,
-  discountType: true,
-  expiresAt: true,
-  limit: true,
-  uses: true,
-  isActive: true,
-  products: { select: { name: true } },
-  _count: { select: { orders: true } },
-};
-
-const getExpiredCouponCodes = () => {
-  return db.couponCode.findMany({
-    select: SELECT_FIELDS,
-    where: WHERE_EXPIRED,
-    orderBy: { createdAt: "asc" },
-  });
-};
-
-export const getUnexpiredCouponCodes = () => {
-  return db.couponCode.findMany({
-    select: SELECT_FIELDS,
-    where: { NOT: WHERE_EXPIRED },
-    orderBy: { createdAt: "asc" },
-  });
-};
+import {
+  getExpiredCouponCodes,
+  getUnexpiredCouponCodes,
+} from "@/services/coupon/couponService";
 
 const CouponPage = async () => {
   const [expiredCouponCodes, unexpiredCouponCodes] = await Promise.all([
