8d323f127c4db394b85f319cf12cf301111860ac
KashifKhn <kashifkhnx04@gmail.com>
2024-08-20 12:01:48 +0500

    feat: Add new charts and components for the admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,77 +1,119 @@
 import DashboardCard from "@/app/admin/_components/DashboardCard";
-import db from "@/db/db";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
 import React from "react";
+import OrdersByDayChart from "./_components/charts/OrdersByDayChart";
+import { getRangeOption, RANGE_OPTIONS } from "@/lib/rangeOptions";
+import ChartCard from "./_components/ChartCard";
+import UsersByDayChart from "./_components/charts/UsersByDayChart";
+import RevenueByProductChart from "./_components/charts/RevenueByProductChart";
+import {
+  getProductData,
+  getSalesData,
+  getUserData,
+} from "@/lib/admin/dashboardUtil";
 
-const getSalesData = async () => {
-  const data = await db.order.aggregate({
-    _sum: { pricePaidInCents: true },
-    _count: true,
-  });
-
-  return {
-    amount: (data._sum.pricePaidInCents || 0) / 100,
-    numberOfSales: data._count,
+type AdminDashboardProps = {
+  searchParams: {
+    totalSalesRange?: string;
+    totalSalesRangeFrom?: string;
+    totalSalesRangeTo?: string;
+    newCustomersRange?: string;
+    newCustomersRangeFrom?: string;
+    newCustomersRangeTo?: string;
+    revenueByProductRange?: string;
+    revenueByProductRangeFrom?: string;
+    revenueByProductRangeTo?: string;
   };
 };
 
-const getUserData = async () => {
-  const [userCount, orderData] = await Promise.all([
-    await db.user.count(),
-    await db.order.aggregate({
-      _sum: { pricePaidInCents: true },
-    }),
-  ]);
+const AdminDashboard = async ({
+  searchParams: {
+    totalSalesRange,
+    totalSalesRangeFrom,
+    totalSalesRangeTo,
+    newCustomersRange,
+    newCustomersRangeFrom,
+    newCustomersRangeTo,
+    revenueByProductRange,
+    revenueByProductRangeFrom,
+    revenueByProductRangeTo,
+  },
+}: AdminDashboardProps) => {
+  const totalSalesRangeOption =
+    getRangeOption(totalSalesRange, totalSalesRangeFrom, totalSalesRangeTo) ||
+    RANGE_OPTIONS.last_7_days;
+  const newCustomersRangeOption =
+    getRangeOption(
+      newCustomersRange,
+      newCustomersRangeFrom,
+      newCustomersRangeTo
+    ) || RANGE_OPTIONS.last_7_days;
+  const revenueByProductRangeOption =
+    getRangeOption(
+      revenueByProductRange,
+      revenueByProductRangeFrom,
+      revenueByProductRangeTo
+    ) || RANGE_OPTIONS.all_time;
 
-  return {
-    userCount: userCount,
-    averageValuePerUser:
-      userCount === 0
-        ? 0
-        : (orderData._sum.pricePaidInCents || 0) / userCount / 100,
-  };
-};
-
-const getProductData = async () => {
-  const [activeCount, inactiveCount] = await Promise.all([
-    db.product.count({ where: { isAvailableForPurchase: true } }),
-    db.product.count({ where: { isAvailableForPurchase: false } }),
-  ]);
-
-  return {
-    activeCount,
-    inactiveCount,
-  };
-};
-
-const AdminDashboard = async () => {
   const [salesData, userData, productData] = await Promise.all([
-    getSalesData(),
-    getUserData(),
-    getProductData(),
+    getSalesData(
+      totalSalesRangeOption.startDate,
+      totalSalesRangeOption.endDate
+    ),
+    getUserData(
+      newCustomersRangeOption.startDate,
+      newCustomersRangeOption.endDate
+    ),
+    getProductData(
+      revenueByProductRangeOption.startDate,
+      revenueByProductRangeOption.endDate
+    ),
   ]);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
-      <DashboardCard
-        title="Sales"
-        subTitle={`${formatNumber(salesData.numberOfSales)} Orders`}
-        body={formatCurrency(salesData.amount)}
-      />
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
+        <DashboardCard
+          title="Sales"
+          subTitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+          body={formatCurrency(salesData.amount)}
+        />
 
-      <DashboardCard
-        title="Customers"
-        subTitle={`${formatCurrency(
-          userData.averageValuePerUser
-        )} Average value`}
-        body={formatNumber(userData.userCount)}
-      />
+        <DashboardCard
+          title="Customers"
+          subTitle={`${formatCurrency(
+            userData.averageValuePerUser
+          )} Average value`}
+          body={formatNumber(userData.userCount)}
+        />
 
-      <DashboardCard
-        title="Active Products"
-        subTitle={`${formatNumber(productData.inactiveCount)} Inactive`}
-        body={formatNumber(productData.activeCount)}
-      />
-    </div>
+        <DashboardCard
+          title="Active Products"
+          subTitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+          body={formatNumber(productData.activeCount)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-8">
+        <ChartCard
+          title="Total Sales"
+          queryKey="totalSalesRange"
+          selectedRangeLabel={totalSalesRangeOption.label}>
+          <OrdersByDayChart data={salesData.chartData} />
+        </ChartCard>
+        <ChartCard
+          title="New Customers"
+          queryKey="newCustomersRange"
+          selectedRangeLabel={newCustomersRangeOption.label}>
+          <UsersByDayChart data={userData.chartData} />
+        </ChartCard>
+        <ChartCard
+          title="Revenue By Product"
+          queryKey="revenueByProductRange"
+          selectedRangeLabel={revenueByProductRangeOption.label}>
+          <RevenueByProductChart data={productData.chartData} />
+        </ChartCard>
+      </div>
+    </>
   );
 };
 
